e123ad83c2834348f3680c83ec32c0b1187de2c9
Nat McConnaughay <natmcconnaughay@gmail.com>
2020-04-30 15:03:31 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def song_by_artist
+    self.artist_name + ' - ' + self.title
+  end
+
 end
